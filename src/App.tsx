bcea91c90cab4ffa1511068fea741b52c3b53fe2
Jonathan Hop <yithril@gmail.com>
2023-03-13 21:50:08 -0400

    I made a change yay!

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <p>This app updates when I do a git commit!</p>
+      <p>This app updates when I do a git commit! I am going to make a change.</p>
     </div>
   );
 }
